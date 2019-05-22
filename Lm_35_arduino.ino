c43e1aef69c8e12d6cc2f93fea3d0635a15599e8
ErezDahan <erezdahan17@gmail.com>
2019-05-22 21:20:06 +0300

    Add files via upload

---
+++ Lm_35_arduino.ino
@@ -0,0 +1,17 @@
+float temp;
+int a;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  temp=analogRead(A0);
+  temp=temp*0.4882125;
+  a=analogRead(A0);
+//  Serial.print("temperature: ");
+//  Serial.print(temp);
+//  Serial.print("*C");
+  
+  Serial.println(temp);
+  delay(20);
+}
